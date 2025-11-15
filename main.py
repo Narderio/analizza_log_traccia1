72848cb143744e91f3d75ad9eecad27b707bd8c7
Dario Nardella <80456732+Narderio@users.noreply.github.com>
2025-11-15 10:19:16 +0100

    valid user id

---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+from utilities.griglia_di_liste_RO import Tabella2D_RO
+
+
+def valid_user_id(colonna: list[object]) -> bool:
+    for elemento in colonna:
+        try:
+            n=int(elemento)
+            if n >= 0:
+                return True
+            else:
+                raise ValueError("L'identificativo dell'utente deve essere un numero intero non negativo")
+        except ValueError:
+            raise ValueError("L'identificativo dell'utente deve essere un numero intero non negativo")
+'''
+def valid_data(colonna: list[object]) -> bool:
+    for elemento in colonna:
+        try:
+            datetime.strptime(elemento, "%d/%m/%Y %H:%M")
+            return True
+        except ValueError:
+            raise ValueError("La data deve essere in formato dd/mm/yyyy hh:mm")
+
+def valid_ip(colonna: list[object]) -> bool:
+    for elemento in colonna:
+        if not isinstance(elemento, str):
+            return False
+    return True
+
+def valid_tabella(tabella: Tabella2D_RO) -> bool:
+    if not valid_user_id(tabella.get_colonna(1)):
+        return False
+    if not valid_data(tabella.get_colonna(0)):
+        return False
+    if not valid_ip(tabella.get_colonna(7)):
+        return False
+    if not valid_others(tabella):
+        return False
+    return True
+'''
+
+if __name__ == "__main__":
+    try:
+        json_data = json.load(open("test_data/test_small.json"))
+    except Exception as e:
+        print("Errore nel caricamento del file JSON: ", e)
+        exit(1)
+
+    
+    tabella = Tabella2D_RO(json_data)
+    print("Dimensioni della tabella: ", tabella.size())
+    print("Elemento (0, 0): ", tabella.get_cell(0, 0))
+    print("Riga 0: ", tabella.get_riga(0))
+    print("Colonna 0: ", tabella.get_colonna(1))
+    print("Validità degli user id: ", valid_user_id(tabella.get_colonna(1)))
+    #valid_tabella(tabella)
+    
